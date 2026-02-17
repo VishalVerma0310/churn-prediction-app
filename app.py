5c55a65972d0c657ed6385d7a55e1537d5427763
Vishal Verma <vermav0310@gmail.com>
2026-02-17 13:22:05 +0530

    Update app.py
    Updated the customer churn prediction app to include encoders for categorical features and added a number input for dependents.

--- app.py
+++ app.py
@@ -3,35 +3,36 @@ import numpy as np
 import pickle
 
 model = pickle.load(open("model.pkl","rb"))
+encoders = pickle.load(open("encoders.pkl","rb"))
 
 st.title("Customer Churn Prediction")
 
-# Numeric inputs
-age = st.number_input("Age", 18, 100)
-tenure = st.number_input("Tenure in Months", 0, 100)
+gender = st.selectbox("Gender", ["Male","Female"])
+age = st.number_input("Age",18,100)
+married = st.selectbox("Married", ["Yes","No"])
+dependents = st.number_input("Number of Dependents",0,10)
+tenure = st.number_input("Tenure in Months",0,100)
+internet = st.selectbox("Internet Service", ["Yes","No"])
+contract = st.selectbox("Contract", ["Month-to-month","One year","Two year"])
+payment = st.selectbox("Payment Method",
+                       ["Electronic check","Mailed check",
+                        "Bank transfer","Credit card"])
 monthly = st.number_input("Monthly Charge")
 total = st.number_input("Total Charges")
 
-# Categorical inputs
-gender = st.selectbox("Gender", ["Male","Female"])
-married = st.selectbox("Married", ["Yes","No"])
-internet = st.selectbox("Internet Service", ["Yes","No"])
-contract = st.selectbox("Contract", ["Month-to-month","One year","Two year"])
-payment = st.selectbox("Payment Method", ["Credit card","Bank transfer","Electronic check"])
+# Encode
+gender = encoders['Gender'].transform([gender])[0]
+married = encoders['Married'].transform([married])[0]
+internet = encoders['Internet Service'].transform([internet])[0]
+contract = encoders['Contract'].transform([contract])[0]
+payment = encoders['Payment Method'].transform([payment])[0]
 
-# Encoding
-gender = 1 if gender=="Male" else 0
-married = 1 if married=="Yes" else 0
-internet = 1 if internet=="Yes" else 0
-
-contract = {"Month-to-month":0,"One year":1,"Two year":2}[contract]
-payment = {"Credit card":0,"Bank transfer":1,"Electronic check":2}[payment]
+input_data = np.array([[gender, age, married,
+                        dependents, tenure,
+                        internet, contract,
+                        payment, monthly, total]])
 
 if st.button("Predict"):
-    input_data = np.array([[age, tenure, monthly, total,
-                            gender, married, internet,
-                            contract, payment]])
-
     pred = model.predict(input_data)
 
     if pred[0]==0:
